83773808c9cb1c6cbfdda3c3ce19133ebf2fb65d
jasonjustin <zxc3.zxc46@gmail.com>
2024-09-18 22:58:09 +0800

    feat:
    1. add target_value arg with default value of $HOME
    2. add --force flag
    3. method of CLI: file_name, file_name_abs, target

--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,52 @@ use std::env;
 use std::path::PathBuf;
 use clap::Parser;
 
+const HOME: &'static str = env!("HOME");
+
 /// A light-weight stow copy
 #[derive(Parser, Debug)]
+#[command(version, about)]
 struct Cli {
     /// Path to the file to symlink
     file_name: PathBuf,
+    /// Target directory
+    #[arg(short, long, value_name = "DIR", default_value = PathBuf::from(HOME).into_os_string())]
+    target_path: PathBuf,
+    /// Force the link if the link already exists
+    #[arg(short, long, default_value_t = false)]
+    force: bool,
+}
+
+impl Cli {
+    fn file_name(&self) -> &std::ffi::OsStr {
+        self.file_name.file_name().unwrap()
+    }
+
+    fn file_name_abs(&self) -> PathBuf {
+        let ret = std::path::absolute(&self.file_name).unwrap();
+        ret
+    }
+
+    fn target(&self) -> PathBuf {
+        self.target_path.join(self.file_name())
+    }
 }
 
 fn main() -> std::io::Result<()> {
-    for arg in env::args() {
-        println!("{arg}");
-    }
-
     let args = Cli::parse();
 
     if !args.file_name.exists() {
         panic!("Error: file not exist: {:?}", args.file_name);
     }
-    let file_name = args.file_name.file_name().unwrap();
-    let home = env::var_os("HOME").unwrap();
 
-    let target_path: PathBuf = PathBuf::from(home).join(file_name);
-    let abs_file_name = std::path::absolute(&args.file_name).unwrap();
+    let file_name_abs = args.file_name_abs();
+    let target: PathBuf = args.target();
 
-    println!("{target_path:?}");
-    match fs::symlink(abs_file_name, target_path) {
+    println!("{target:?}");
+    match fs::symlink(file_name_abs, target) {
         Ok(()) => (),
         Err(error) => match error.kind() {
-            ErrorKind::AlreadyExists => panic!("Symlink already exists."),
+            ErrorKind::AlreadyExists => panic!("Symlink already exists. Use flag -f or --force for force link."),
             other_error => panic!("Error: cannot create symlink: {other_error:?}")
         }
     }
